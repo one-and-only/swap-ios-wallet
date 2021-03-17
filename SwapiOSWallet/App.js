8aa069a7ce9c4ae4fbe65a906a67e59332c4dc54
Antonios Papadakis <1001545@isd535.org>
2021-03-17 09:06:00 -0500

    Initialize App

---
+++ SwapiOSWallet/App.js
@@ -0,0 +1,20 @@
+/**
+ * Swap iOS Wallet
+ * https://github.com/one-and-only/swap-ios-wallet
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+
+const SwapWallet = () => {
+  return (
+    <View>
+      <Text>This is some text</Text>
+    </View>
+  );
+}
+
+export default SwapWallet;

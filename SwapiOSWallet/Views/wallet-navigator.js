955f26ccf0118ab1bd4b321fc6330f383319eb2b
Antonios Papadakis <1001545@isd535.org>
2021-06-03 07:31:08 -0500

    Update Styling for the Wallet Drawer Navigator
    Color scheme updates, font and border radius updates

--- SwapiOSWallet/Views/wallet-navigator.js
+++ SwapiOSWallet/Views/wallet-navigator.js
@@ -2,10 +2,20 @@ import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
+import { Dimensions, } from 'react-native';
+
 import SwapWallet from './Wallet/wallet';
 import SwapTransactions from './Wallet/transactions';
 import SwapSend from './Wallet/send';
 import SwapSettings from './Wallet/settings';
+
+const {width, height} = Dimensions.get('window');
+const widthScale = width/375;
+
+// normalize the input so that it scales evenly across devices
+function normalize (pre) {
+    return Math.floor(pre * widthScale);
+}
 
 export default class SwapWalletHome extends React.Component {
     constructor(props) {
@@ -14,10 +24,25 @@ export default class SwapWalletHome extends React.Component {
 
     render() {
         const Drawer = createDrawerNavigator();
+        const drawerContentOptions = {
+            style: {
+                backgroundColor: '#0a478a',
+            },
+            itemStyle: {
+                borderRadius: 15,
+            },
+            labelStyle: {
+                color: 'white',
+                fontWeight: '700',
+                fontSize: normalize(18),
+            },
+            inactiveBackgroundColor: "#1066c2",
+            activeBackgroundColor: '#a260f8',
+        };
 
         return (
             <NavigationContainer independent={true}>
-                <Drawer.Navigator initialRouteName="Home">
+                <Drawer.Navigator initialRouteName="Home" drawerContentOptions={drawerContentOptions}>
                     <Drawer.Screen name="Wallet" navigation={this.props.navigation} component={SwapWallet}/>
                     <Drawer.Screen name="Transactions" navigation={this.props.navigation} component={SwapTransactions}/>
                     <Drawer.Screen name="Send" navigation={this.props.navigation} component={SwapSend}/>

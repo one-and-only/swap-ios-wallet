bd243a9324d619c555894cd9483134329de7f3c4
Antonios Papadakis <bdpaboi123@gmail.com>
2021-12-24 13:06:37 -0600

    Enable Hermes and Bump Version

--- SwapiOSWallet/App.js
+++ SwapiOSWallet/App.js
@@ -63,7 +63,7 @@ class Navigator extends React.Component {
 
 		const headerLeft = () => {
 			return (
-				<TouchableOpacity onPress={() => {Alert.alert("About", "Swap Mobile Wallet v0.12.1\n\n©2021 Antonios Papadakis");}}>
+				<TouchableOpacity onPress={() => {Alert.alert("About", "Swap Mobile Wallet v0.13.0\n©2021 Antonios Papadakis");}}>
 					<FontAwesome5 style={styles.infoIcon} size={normalize(30)} name={"info-circle"} color={"white"} />
 				</TouchableOpacity>
 			);

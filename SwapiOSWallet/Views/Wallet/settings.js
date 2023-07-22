894e646016c5050f3b4b402b04a69799c2cca407
Antonios Papadakis <bdpaboi123@gmail.com>
2023-07-22 16:00:14 +0300

    Remove Spend Key Editability in Settings

--- SwapiOSWallet/Views/Wallet/settings.js
+++ SwapiOSWallet/Views/Wallet/settings.js
@@ -42,22 +42,14 @@ export default class SwapSettings extends React.Component {
 			enableAnalytics: false,
 		};
 
-		const spendKeySec = Settings.select("spendKey_sec");
-		const spendKeyPub = Settings.select("spendKey_pub");
 		const enableAnalytics = Settings.select("enableAnalytics");
 
-		Promise.all([spendKeySec, spendKeyPub, enableAnalytics]).then((settings) => {
-			let spendKey_sec;
-			let spendKey_pub;
+		Promise.all([enableAnalytics]).then((settings) => {
 			let enableAnalytics;
 
-			(settings[0] != null) ? spendKey_sec = settings[0] : spendKey_sec = "";
-			(settings[1] != null) ? spendKey_pub = settings[1] : spendKey_pub = "";
-			(settings[2] != null) ? enableAnalytics = JSON.parse(settings[2]) : enableAnalytics = true;
+			(settings[0] != null) ? enableAnalytics = JSON.parse(settings[0]) : enableAnalytics = true;
 
 			this.setState({
-				spendKey_sec: spendKey_sec,
-				spendKey_pub: spendKey_pub,
 				enableAnalytics: enableAnalytics,
 			});
 		});
@@ -67,23 +59,11 @@ export default class SwapSettings extends React.Component {
 		return (
 			<ScrollView contentContainerStyle={styles.mainView}>
 				<View style={{ flexDirection: "row", marginTop: normalize(10, widthScale), }}>
-					<Text style={styles.text}>Spend Key (Private):</Text>
-					<View style={[styles.flexContainerChild, { marginLeft: normalize(10, widthScale), }]}>
-						<TextInput style={[styles.text, styles.textBox]} underlineColorAndroid='transparent' placeholder='Spend Key (Private)' placeholderTextColor='#c9c9c9' defaultValue={this.state.spendKey_sec} autoCapitalize='none' onChangeText={handleSpendKeySec}></TextInput>
-					</View>
-				</View>
-				<View style={{ flexDirection: "row", marginTop: normalize(10, widthScale), }}>
-					<Text style={styles.text}>Spend Key (Public):</Text>
-					<View style={[styles.flexContainerChild, { marginLeft: normalize(10, widthScale), }]}>
-						<TextInput style={[styles.text, styles.textBox]} underlineColorAndroid='transparent' placeholder='Spend Key (Public)' placeholderTextColor='#c9c9c9' defaultValue={this.state.spendKey_pub} autoCapitalize='none' onChangeText={handleSpendKeyPub}></TextInput>
-					</View>
-				</View>
-				<View style={{ flexDirection: "row", marginTop: normalize(10, widthScale), }}>
 					<Text style={styles.text}>Enable Analytics:</Text>
 					<View style={[styles.flexContainerChild, { marginLeft: normalize(10, widthScale), }]}>
 						<Switch
 							trackColor={{ false: "#767577", true: "#55a1f3" }}
-							thumbColor={this.state.enableAnalytics ? "#a065fa" : "#a065fa"}
+							thumbColor={"#a065fa"}
 							ios_backgroundColor="#3e3e3e"
 							onValueChange={() => { this.setState({ enableAnalytics: !this.state.enableAnalytics }); handleEnableAnalytics(!this.state.enableAnalytics); }}
 							value={this.state.enableAnalytics}
